21608990ad304839779d09d3d431c56844aa2acb
Stefun Hawkins <stefunhawkins@zipcoders-MacBook-Pro-8.local>
2019-06-01 21:41:17 -0400

    finished

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,16 +10,23 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+        boolean result = false;
+        if (x > y) {
+            result = true;
+        }
+        return result;
     }
-
     /**
      * @param x
      * @param y
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+        boolean result = false;
+        if (x < y) {
+            result = true;
+        }
+        return result;
     }
 
     /**
@@ -28,8 +35,13 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+        boolean result = false;
+        if (x >= y) {
+            result = true;
+        }
+        return result;
     }
+
 
     /**
      * @param x
@@ -37,22 +49,27 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+        boolean result = false;
+        if (x <= y) {
+            result = true;
+        }
+        return result;
     }
+
 
 
     /**
      * @return true
      */
     public Boolean returnTrue() {
-        return null;
+        return true;
     }
 
     /**
      * @return false
      */
     public Boolean returnFalse() {
-        return null;
+        return false;
     }
 
 }
